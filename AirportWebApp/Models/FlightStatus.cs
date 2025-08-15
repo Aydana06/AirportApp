1b8926ae547db65b9a70aea159c7a395f90cb232
Aydana06 <bolataidana73@gmail.com>
2025-08-15 14:19:10 +0700

    webApp

---
+++ AirportWebApp/Models/FlightStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AirportWebApp.Models
+{
+    public class FlightStatus
+    {
+        public string FlightNumber { get; set; }
+        public string Destination { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public string Gate { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ AirportWebApp/Pages/Departure.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using AirportWebApp.Models;
+using System;
+using System.Collections.Generic;
+
+namespace AirportWebApp.Pages
+{
+    public class DepartureModel : PageModel
+    {
+        public List<FlightStatus> Flights { get; set; }
+
+        public void OnGet()
+        {
+            // Жишээ өгөгдөл
+            Flights = new List<FlightStatus>
+            {
+                new FlightStatus { FlightNumber = "OM301", Destination = "Ulaanbaatar", DepartureTime = DateTime.Now.AddMinutes(45), Gate = "A12", Status = "On Time" },
+                new FlightStatus { FlightNumber = "OM402", Destination = "Seoul", DepartureTime = DateTime.Now.AddHours(1), Gate = "B05", Status = "Boarding" },
+                new FlightStatus { FlightNumber = "OM501", Destination = "Tokyo", DepartureTime = DateTime.Now.AddMinutes(-10), Gate = "C07", Status = "Departed" },
+                new FlightStatus { FlightNumber = "OM207", Destination = "Beijing", DepartureTime = DateTime.Now.AddMinutes(30), Gate = "A04", Status = "Delayed" }
+            };
+        }
+    }
+}
